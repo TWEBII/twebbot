e2924880544a33cb2c5e8190030c8a73373292a2
TWEBII <ahmed3radam3@gmail.com>
2026-07-29 14:36:57 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,26 @@
+import telebot
 
+from config import BOT_TOKEN
+from handlers import register_handlers
+
+if not BOT_TOKEN:
+    raise Exception("❌ BOT_TOKEN غير موجود، أضفه في ملف .env أو Railway Variables")
+
+bot = telebot.TeleBot(
+    BOT_TOKEN,
+    parse_mode="Markdown"
+)
+
+register_handlers(bot)
+
+print("===================================")
+print("🤖 Tweby AI Started Successfully")
+print("===================================")
+
+bot.remove_webhook()
+
+bot.infinity_polling(
+    skip_pending=True,
+    timeout=60,
+    long_polling_timeout=60
+)
